d9864c353c8122961c2d1542fa11129d6ffc7e88
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2024-01-16 15:08:17 +0530

    added active and deactive nav link on scroll or on click

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 
+import { useEffect } from 'react';
 import './App.css';
 import Footer from './copmonents/footer/footer';
 import Header from './copmonents/header/header';
@@ -12,6 +13,34 @@ import Skills from './sections/skills';
 import Testimonial from './sections/testimonial';
 
 function App() {
+  
+    useEffect(()=>{
+      let sections = document.querySelectorAll("section");
+  window.onscroll = () => {
+    
+    let scrollY = window.pageYOffset;
+  
+    sections.forEach((element )=> {
+      
+      let sectionHeight = element.offsetHeight
+      let sectionTop = element.offsetTop - 30
+      let sectionId = element.getAttribute("id")
+      let sectionClass = document.querySelector(`#nav li a[href*=${sectionId}]`)
+
+      // remove or hide nav bar on scroll
+      document.getElementById("nav").classList.remove("active_nav")
+
+      if( scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+          sectionClass?.classList.add("active_link")
+      }
+      else{
+        sectionClass?.classList.remove("active_link")
+      }
+
+    })
+  }
+},[])
+
   return (
     <div className="App">
       <Header/>
